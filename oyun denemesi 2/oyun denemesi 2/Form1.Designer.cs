852eb21b2d4d95e36622fe6dd3d6f51d2211a09d
Yunus Gümüş <emregms2009@gmail.com>
2025-12-01 09:08:17 +0300

    Geri-donusum-çevre-bilinclendirme-oyunu
    Bu dairesel uygulama çevre bilinçlendirme ve eğitim odaklı bir uygulama ama görsel stüdyosunun yetersiz oyun desteğinden dolayı yarım kalan ama temelleri sağlam olan bir oyun kodları yapay zeka tarafından düzenlenmiş ve açıklama eklenmiştir

---
+++ oyun denemesi 2/oyun denemesi 2/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace oyun_denemesi_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            myhero = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            label1 = new Label();
+            pictureBox12 = new PictureBox();
+            copKutusu = new PictureBox();
+            copProgressBar = new ProgressBar();
+            suKaynagi = new PictureBox();
+            agac = new PictureBox();
+            suProgressBar = new ProgressBar();
+            agacProgressBar = new ProgressBar();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)myhero).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)copKutusu).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)suKaynagi).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)agac).BeginInit();
+            SuspendLayout();
+            // 
+            // myhero
+            // 
+            myhero.Image = Properties.Resources.character_idle_1_1;
+            myhero.Location = new Point(75, 364);
+            myhero.Name = "myhero";
+            myhero.Size = new Size(126, 158);
+            myhero.SizeMode = PictureBoxSizeMode.Zoom;
+            myhero.TabIndex = 3;
+            myhero.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.duzuzun;
+            pictureBox4.Location = new Point(17, 517);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(502, 260);
+            pictureBox4.TabIndex = 7;
+            pictureBox4.TabStop = false;
+            pictureBox4.Tag = "platform";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.duzuzun;
+            pictureBox5.Location = new Point(493, 517);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(501, 260);
+            pictureBox5.TabIndex = 8;
+            pictureBox5.TabStop = false;
+            pictureBox5.Tag = "platform";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.duzuzun;
+            pictureBox6.Location = new Point(963, 517);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(601, 260);
+            pictureBox6.TabIndex = 9;
+            pictureBox6.TabStop = false;
+            pictureBox6.Tag = "platform";
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = Properties.Resources.diken;
+            pictureBox8.Location = new Point(1260, 402);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(145, 47);
+            pictureBox8.TabIndex = 11;
+            pictureBox8.TabStop = false;
+            pictureBox8.Tag = "diken";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.ForeColor = Color.DarkGoldenrod;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 24);
+            label1.TabIndex = 13;
+            label1.Text = "Para:";
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Image = Properties.Resources.coin1;
+            pictureBox12.Location = new Point(1078, 379);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(53, 70);
+            pictureBox12.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox12.TabIndex = 16;
+            pictureBox12.TabStop = false;
+            pictureBox12.Tag = "coin";
+            // 
+            // copKutusu
+            // 
+            copKutusu.Image = (Image)resources.GetObject("copKutusu.Image");
+            copKutusu.Location = new Point(786, 412);
+            copKutusu.Name = "copKutusu";
+            copKutusu.Size = new Size(100, 110);
+            copKutusu.SizeMode = PictureBoxSizeMode.Zoom;
+            copKutusu.TabIndex = 17;
+            copKutusu.TabStop = false;
+            copKutusu.Tag = "cop";
+            // 
+            // copProgressBar
+            // 
+            copProgressBar.Location = new Point(786, 341);
+            copProgressBar.Name = "copProgressBar";
+            copProgressBar.Size = new Size(100, 23);
+            copProgressBar.TabIndex = 18;
+            // 
+            // suKaynagi
+            // 
+            suKaynagi.Image = Properties.Resources.suKaynagı;
+            suKaynagi.Location = new Point(619, 379);
+            suKaynagi.Name = "suKaynagi";
+            suKaynagi.Size = new Size(100, 143);
+            suKaynagi.SizeMode = PictureBoxSizeMode.Zoom;
+            suKaynagi.TabIndex = 19;
+            suKaynagi.TabStop = false;
+            // 
+            // agac
+            // 
+            agac.Image = Properties.Resources.agac;
+            agac.Location = new Point(328, 312);
+            agac.Name = "agac";
+            agac.Size = new Size(144, 210);
+            agac.SizeMode = PictureBoxSizeMode.Zoom;
+            agac.TabIndex = 20;
+            agac.TabStop = false;
+            // 
+            // suProgressBar
+            // 
+            suProgressBar.Location = new Point(73, 51);
+            suProgressBar.Name = "suProgressBar";
+            suProgressBar.Size = new Size(100, 23);
+            suProgressBar.TabIndex = 21;
+            // 
+            // agacProgressBar
+            // 
+            agacProgressBar.Location = new Point(349, 283);
+            agacProgressBar.Name = "agacProgressBar";
+            agacProgressBar.Size = new Size(100, 23);
+            agacProgressBar.TabIndex = 22;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.ForeColor = Color.FromArgb(0, 0, 192);
+            label2.Location = new Point(12, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 24);
+            label2.TabIndex = 23;
+            label2.Text = "Su =";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightCyan;
+            ClientSize = new Size(1468, 825);
+            Controls.Add(myhero);
+            Controls.Add(label2);
+            Controls.Add(suProgressBar);
+            Controls.Add(pictureBox12);
+            Controls.Add(label1);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox6);
+            Controls.Add(agac);
+            Controls.Add(suKaynagi);
+            Controls.Add(copKutusu);
+            Controls.Add(copProgressBar);
+            Controls.Add(agacProgressBar);
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ((System.ComponentModel.ISupportInitialize)myhero).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)copKutusu).EndInit();
+            ((System.ComponentModel.ISupportInitialize)suKaynagi).EndInit();
+            ((System.ComponentModel.ISupportInitialize)agac).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox myhero;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox8;
+        private Label label1;
+        private PictureBox pictureBox12;
+        private PictureBox copKutusu;
+        private ProgressBar copProgressBar;
+        private PictureBox suKaynagi;
+        private PictureBox agac;
+        private ProgressBar suProgressBar;
+        private ProgressBar agacProgressBar;
+        private Label label2;
+    }
+}
---
+++ oyun denemesi 2/oyun denemesi 2/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oyun_denemesi_2
+{
+    public partial class Form1 : Form
+    {
+        int skor = 0;
+        List<PictureBox> coinler = new List<PictureBox>();
+        List<PictureBox> dikenler = new List<PictureBox>();
+        List<PictureBox> platformlar = new List<PictureBox>();
+
+        bool sagaBasildi = false;
+        bool solaBasildi = false;
+        bool yerdeMi = false;
+
+        int hareketHizi = 7;
+        int gravity = 2;
+        int yerSeviyesi = 300;
+
+        bool ziplaniyor = false;
+        int ziplamaHizi = 0;
+        int ziplamaGucu = -20;
+
+        private System.Windows.Forms.Timer bekleTimer = new System.Windows.Forms.Timer();
+
+        string sonYön = "sag";
+
+        // Çöp kutusu
+        int copTemizlikSeviye = 0;
+        int copTemizlemeSiniri = 5;
+
+        // Su ve ağaç sistemi
+        int suMiktari = 0;
+        int agacSulamaSeviye = 0;
+        int maxSulamaSeviye = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Platform, diken ve coinleri toplama
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is PictureBox pb && pb.Tag != null)
+                {
+                    if (pb.Tag.ToString() == "platform") platformlar.Add(pb);
+                    else if (pb.Tag.ToString() == "diken") dikenler.Add(pb);
+                    else if (pb.Tag.ToString() == "coin") coinler.Add(pb);
+                }
+            }
+
+            // Etkileşim olayları
+            copKutusu.Click += CopKutusu_Click;
+            suKaynagi.Click += SuKaynagi_Click;
+            agac.Click += Agac_Click;
+
+            bekleTimer.Interval = 1000;
+            bekleTimer.Tick += bekleTimer_Tick;
+        }
+
+        private async void KarakteriOldur()
+        {
+            sagaBasildi = false;
+            solaBasildi = false;
+
+            myhero.Image = Image.FromFile("hero_death.png");
+            await Task.Delay(1000);
+
+            myhero.Top = 81;
+            myhero.Left = 50;
+            ziplamaHizi = 0;
+            ziplaniyor = false;
+            sonYön = "sag";
+            myhero.Image = Image.FromFile("hero0.png");
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            // === SAĞ ===
+            if (sagaBasildi)
+            {
+                Rectangle yeniAlan = myhero.Bounds;
+                yeniAlan.X += hareketHizi;
+
+                if (!platformlar.Any(p => yeniAlan.IntersectsWith(p.Bounds)))
+                {
+                    myhero.Left += hareketHizi;
+                    myhero.Image = Image.FromFile("hero_run0.png");
+                    sonYön = "sag";
+                }
+            }
+
+            // === SOL ===
+            if (solaBasildi)
+            {
+                Rectangle yeniAlan = myhero.Bounds;
+                yeniAlan.X -= hareketHizi;
+
+                if (!platformlar.Any(p => yeniAlan.IntersectsWith(p.Bounds)))
+                {
+                    myhero.Left -= hareketHizi;
+                    myhero.Image = Image.FromFile("hero_run1.png");
+                    sonYön = "sol";
+                }
+            }
+
+            // === Zıplama ===
+            if (ziplaniyor)
+            {
+                myhero.Image = Image.FromFile("hero_jump_0.png");
+                myhero.Top += ziplamaHizi;
+                ziplamaHizi += gravity;
+
+                if (ziplamaHizi > 10)
+                    ziplaniyor = false;
+            }
+            else if (!yerdeMi)
+            {
+                myhero.Top += gravity;
+            }
+
+            // === Platform çarpışması ===
+            bool carpti = false;
+            foreach (var platform in platformlar)
+            {
+                if (myhero.Bounds.IntersectsWith(platform.Bounds))
+                {
+                    if (myhero.Bottom >= platform.Top &&
+                        myhero.Top < platform.Top &&
+                        myhero.Right > platform.Left &&
+                        myhero.Left < platform.Right)
+                    {
+                        myhero.Top = platform.Top - myhero.Height;
+                        ziplaniyor = false;
+                        ziplamaHizi = 0;
+                        carpti = true;
+                        break;
+                    }
+                }
+            }
+            yerdeMi = carpti;
+
+            // === Diken çarpışması ===
+            foreach (var diken in dikenler)
+            {
+                if (myhero.Bounds.IntersectsWith(diken.Bounds))
+                {
+                    KarakteriOldur();
+                    return;
+                }
+            }
+
+            // === Coin çarpışması ===
+            foreach (var coin in coinler.ToList())
+            {
+                if (myhero.Bounds.IntersectsWith(coin.Bounds))
+                {
+                    skor += 10;
+                    label1.Text = "Para: " + skor;
+                    this.Controls.Remove(coin);
+                    coinler.Remove(coin);
+                    break;
+                }
+            }
+
+            // === Durma animasyonu ===
+            if (!sagaBasildi && !solaBasildi && yerdeMi && !ziplaniyor)
+            {
+                if (sonYön == "sag")
+                    myhero.Image = Image.FromFile("hero0.png");
+                else
+                    myhero.Image = Image.FromFile("hero1.png");
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) sagaBasildi = true;
+            else if (e.KeyCode == Keys.Left) solaBasildi = true;
+
+            if (e.KeyCode == Keys.Space && yerdeMi && !ziplaniyor)
+            {
+                ziplaniyor = true;
+                ziplamaHizi = ziplamaGucu;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) sagaBasildi = false;
+            else if (e.KeyCode == Keys.Left) solaBasildi = false;
+        }
+
+        private void bekleTimer_Tick(object sender, EventArgs e)
+        {
+            bekleTimer.Stop();
+        }
+
+        // ========================= Çöp Kutusu =========================
+        private void CopKutusu_Click(object sender, EventArgs e)
+        {
+            int mesafe = Math.Abs(myhero.Left - copKutusu.Left);
+            if (mesafe > 200)
+            {
+                MessageBox.Show("Çöp kutusuna yaklaşmalısın!");
+                return;
+            }
+
+            if (copTemizlikSeviye < copTemizlemeSiniri)
+            {
+                copTemizlikSeviye++;
+                copProgressBar.Value = copTemizlikSeviye * (100 / copTemizlemeSiniri);
+                string resimYolu = $"cop{copTemizlikSeviye}.png";
+                if (System.IO.File.Exists(resimYolu))
+                    copKutusu.Image = Image.FromFile(resimYolu);
+
+                if (copTemizlikSeviye == copTemizlemeSiniri)
+                    MessageBox.Show("Çöp kutusu tamamen temizlendi! ✨");
+            }
+        }
+
+        // ========================= Su Alma =========================
+        private void SuKaynagi_Click(object sender, EventArgs e)
+        {
+            int mesafe = Math.Abs(myhero.Left - suKaynagi.Left);
+            if (mesafe > 200)
+            {
+                MessageBox.Show("Su almak için yaklaşırsan iyi olur!");
+                return;
+            }
+
+            if (suMiktari < maxSulamaSeviye)
+            {
+                suMiktari++;
+                suProgressBar.Value = suMiktari * (100 / maxSulamaSeviye);
+            }
+        }
+
+        // ========================= Ağaç Sulama =========================
+        private void Agac_Click(object sender, EventArgs e)
+        {
+            int mesafe = Math.Abs(myhero.Left - agac.Left);
+            if (mesafe > 200)
+            {
+                MessageBox.Show("Ağacı sulamak için daha da yaklaş!");
+                return;
+            }
+
+            if (suMiktari > 0 && agacSulamaSeviye < maxSulamaSeviye)
+            {
+                suMiktari--;
+                suProgressBar.Value = suMiktari * (100 / maxSulamaSeviye);
+
+                agacSulamaSeviye++;
+                agacProgressBar.Value = agacSulamaSeviye * (100 / maxSulamaSeviye);
+
+                if (agacSulamaSeviye == maxSulamaSeviye)
+                    MessageBox.Show("Ağaç tamamen sulandı! 🌳");
+            }
+            else if (suMiktari == 0)
+            {
+                MessageBox.Show("Önce su almanız gerekiyor!");
+            }
+        }
+    }
+}
